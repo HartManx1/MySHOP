14584206eba4a616aa96c005cd6a4e985af98c20
Martin Hartman <hartmanx1@wp.pl>
2019-11-12 07:41:33 +0100

    v3

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter basename="/MySHOP/">
+      <BrowserRouter>
 
         <MainLayout>
           <ProductProvider>
